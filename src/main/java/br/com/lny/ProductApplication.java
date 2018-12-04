0bacfbf26707541ada7b27b93a42ba966a97c71f
Leo Yassuda <leonardo.yassuda@kestraa.com>
2018-12-04 08:55:36 -0200

    update maven dependencies

--- src/main/java/br/com/lny/ProductApplication.java
+++ src/main/java/br/com/lny/ProductApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+@EnableOAuth2Client
 public class ProductApplication {
 
     public static void main(String[] args) {
--- src/test/java/br/com/lny/ProductApplicationTest.java
+++ src/test/java/br/com/lny/ProductApplicationTest.java
@@ -2,13 +2,19 @@ package br.com.lny;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,8 +22,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,58 +41,84 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public class ProductApplicationTest {
 
-    private static final Logger logger = LogManager.getLogger(ProductApplicationTest.class);
+	private static final Logger logger = LogManager.getLogger(ProductApplicationTest.class);
 
-    @Rule
-    public OutputCapture output = new OutputCapture();
+	@Rule
+	public OutputCapture output = new OutputCapture();
 
-    @Autowired
-    private WebApplicationContext context;
+	@Autowired
+	private WebApplicationContext context;
 
-    private MockMvc mvc;
+	private MockMvc mvc;
 
-    @Autowired
-    private ApplicationContext applicationContext;
+	@Autowired
+	private ApplicationContext applicationContext;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+	@Autowired
+	private TestRestTemplate restTemplate;
 
-    @Before
-    public void setUp() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-    }
+	private URL base;
 
-    @Test
-    public void testLogger() {
-        logger.info("Hello World - Teste de log4j");
-        this.output.expect(containsString("Hello World - Teste de log4j"));
-    }
+	@LocalServerPort
+	int port;
 
-    @Ignore
-    public void validateLoggersEndpoint() throws Exception {
-        this.mvc.perform(get("/application/loggers/org.apache.coyote.http11.Http11NioProtocol"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("{\"configuredLevel\":\"WARN\","
-                        + "\"effectiveLevel\":\"WARN\"}")));
-    }
+	@Before
+	public void setUp() throws MalformedURLException {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		restTemplate = new TestRestTemplate("user", "password");
+		base = new URL("http://localhost:" + port);
+	}
 
-    @Test
-    public void verifyStatus() {
-        String body = this.restTemplate.getForObject("/", String.class);
-        Assert.assertEquals(body.toUpperCase(), "OK");
-    }
+	@Test
+	public void whenLoggedUserRequestsHomePage_ThenSuccess()
+			throws IllegalStateException, IOException {
+		ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
 
-    @Test
-    public void verifyHealth() throws Exception {
-        this.mvc.perform(get("/actuator/health"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"status\":\"UP\"")));
-    }
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(response.getBody().contains("ok"));
+	}
 
-    @Test
-    public void verifyGetProductSpecific() throws Exception {
-        this.mvc.perform(get("/api/product/1"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"name\":\"Product 1\"")));
-    }
+	@Test
+	public void whenUserWithWrongCredentials_thenUnauthorizedPage() throws Exception {
+
+		this.restTemplate = new TestRestTemplate("user", "wrongpassword");
+		ResponseEntity<String> response = this.restTemplate.getForEntity(this.base.toString(), String.class);
+
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+		assertTrue(Objects.requireNonNull(response.getBody()).contains("Unauthorized"));
+	}
+
+	@Test
+	public void testLogger() {
+		logger.info("Hello World - Teste de log4j");
+		this.output.expect(containsString("Hello World - Teste de log4j"));
+	}
+
+	@Ignore
+	public void validateLoggersEndpoint() throws Exception {
+		this.mvc.perform(get("/application/loggers/org.apache.coyote.http11.Http11NioProtocol"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("{\"configuredLevel\":\"WARN\","
+						+ "\"effectiveLevel\":\"WARN\"}")));
+	}
+
+	@Test
+	public void verifyStatus() {
+		String body = this.restTemplate.getForObject(this.base.toString(), String.class);
+		assertEquals(body.toUpperCase(), "OK");
+	}
+
+	@Test
+	public void verifyHealth() throws Exception {
+		this.mvc.perform(get("/actuator/health"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("\"status\":\"UP\"")));
+	}
+
+	@Test
+	public void verifyGetProductSpecific() throws Exception {
+		this.mvc.perform(get("/api/product/1"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("\"name\":\"Product 1\"")));
+	}
 }
