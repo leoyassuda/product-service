7a9aea4ceafad7fa8fac8a49ba2dd96db3c21165
Leo Yasuuda <leo.yassuda@gmail.com>
2019-07-29 17:15:05 -0300

    retirado stack de cloud para deixar apenas um template spring.

--- src/main/java/br/com/lny/ProductApplication.java
+++ src/main/java/br/com/lny/ProductApplication.java
@@ -3,16 +3,12 @@ package br.com.lny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
-@EnableCircuitBreaker
 @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class ProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
 
 }
--- src/main/java/br/com/lny/config/RestTemplateConfig.java
+++ src/main/java/br/com/lny/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package br.com.lny.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,11 +7,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
-	// Create a bean for restTemplate to call services
-	@Bean
-	@LoadBalanced // Load balance between service instances running at different ports.
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    // Create a bean for restTemplate to call services
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
